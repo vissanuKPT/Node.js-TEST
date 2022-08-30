4fd209627c673dd3a1857786a099a28b6ca7eb34
vissanuKPT <112517485+vissanuKPT@users.noreply.github.com>
2022-08-31 03:48:31 +0700

    1K

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) => {
+
+    res.send('Hello KK');
+
+})
+
+app.listen(port, ()=>{
+    debug("Listening on port" + chalk.green(port));
+
+})
