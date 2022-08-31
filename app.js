072fab85199fe6184f2f1544f52036ad7fc20eed
vissanuKPT <112517485+vissanuKPT@users.noreply.github.com>
2022-08-31 23:52:19 +0700

    5k

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productsRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,6 +13,17 @@ app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine", "ejs")
+
+productsRouter.route("/").get((req,res) =>{
+    res.send("HI Go Go");
+});
+
+productsRouter.route("/1").get((req,res) =>{
+    res.send("HI Go Go 1");
+});
+
+
+app.use("/products",productsRouter)
 
 app.get("/", (req,res) => {
 
